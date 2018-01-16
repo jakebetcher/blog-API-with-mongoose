095febde374ffff192a697a30a2914880a4ce012
Jake Betcher <jakebetcher@gmail.com>
2018-01-16 11:00:46 -0800

    add schema for blog posts

---
+++ models.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const blogPostSchema = mongoose.Schema({
+  title: {type: String, required: true},
+  content: {type: String, required: true},
+  author: {type: String, required: true},
+  publishDate: Date
+});
+
+blogPostSchema.Schema.methods.serialize = function() {
+
+  return {
+    id: this._id,
+    title: this.title,
+    content: this.content,
+    author: this.author,
+    publishDate: this.publishDate
+  };
+}
+
+const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+
+module.exports = {BlogPost};
